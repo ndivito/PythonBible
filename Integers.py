d86f1c5474e46fc19ce071c281eeed8998fdac26
Nicholas DiVito <ndivito7@gmail.com>
2023-01-31 18:15:44 -0600

    add integers challenge and main file

---
+++ Integers.py
@@ -0,0 +1,4 @@
+# In this challenge, I want you to create a variable called x
+# Set the value for x equal to any whole number you choose using the assignment operator
+
+x = 10 # this will accomplish the asked task
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+first_number = 1+1
+print(first_number)
+second_number = 105+10
+print(second_number)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+total = first_number + second_number
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(total)
