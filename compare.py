64c3b3c30c066683e8d75f9f801dc18bb7a97fe3
Nicholas DiVito <ndivito7@gmail.com>
2023-02-05 19:27:09 -0600

    add all section 6: last commit was actually section 5

---
+++ compare.py
@@ -0,0 +1,11 @@
+num1 =  50
+num2 =  51
+
+if num1 > num2:  # Replace CONDITION1 with a valid condition that makes this work
+    print("num1 is bigger than num2")
+
+elif num2 == num1:  # Replace CONDITION2 with a valid condition that makes this work
+    print("num1 is equal to num2")
+
+elif num1 < num2:  # Replace CONDITION3 with a valid condition that makes this work
+    print("num2 is bigger than num1")
